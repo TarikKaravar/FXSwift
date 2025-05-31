48936990e75e176d670bbbaf6964f318b0a53d75
Tarık Karavar <tarikkaravar17@gmail.com>
2025-05-31 22:02:39 +0300

    haber sayfası düzenlendi

--- lib/models/news_article.dart
+++ lib/models/news_article.dart
@@ -2,18 +2,21 @@ class NewsArticle {
   final String title;
   final String url;
   final String source;
+  final String imageUrl;
 
   NewsArticle({
     required this.title,
     required this.url,
     required this.source,
+    required this.imageUrl,
   });
 
   factory NewsArticle.fromJson(Map<String, dynamic> json) {
     return NewsArticle(
       title: json['title'] ?? '',
       url: json['url'] ?? '',
-      source: json['source']?['name'] ?? 'Bilinmeyen',
+      source: json['source']['name'] ?? '',
+      imageUrl: json['urlToImage'] ?? '',
     );
   }
 }
